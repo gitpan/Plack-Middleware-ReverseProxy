b5f007143763d887e688ff0a4d10014008c228d5
Nobuo Danjou <danjou@soffritto.org>
2009-12-22 08:17:51 +0000

    Import of DANJOU/Plack-Middleware-ReverseProxy-0.03 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-ReverseProxy
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         DANJOU/Plack-Middleware-ReverseProxy-0.03.tar.gz
    gitpan-cpan-author:       DANJOU
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/ReverseProxy.pm
+++ lib/Plack/Middleware/ReverseProxy.pm
@@ -2,8 +2,9 @@ package Plack::Middleware::ReverseProxy;
 
 use strict;
 use warnings;
+use 5.008_001;
 use parent qw(Plack::Middleware);
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub call {
     my $self = shift;
